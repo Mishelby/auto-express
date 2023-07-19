0a58a32c1b2aff122922877c8eebceb19f82e0a6
Mishelby <lorestatys@Yandex.ru>
2023-07-19 17:54:08 +0300

    finish adaptive

--- build/js/slider.bundle.js
+++ build/js/slider.bundle.js
@@ -1,1 +1,1 @@
-$((function(){$(".auto-slider").slick({dots:!0,slidesToShow:3,slidesToScroll:1,prevArrow:!1,nextArrow:!1})}));
+$((function(){$(".auto-slider").slick({dots:!0,slidesToShow:3,slidesToScroll:1,prevArrow:!1,nextArrow:!1,responsive:[{breakpoint:841,settings:{slidesToShow:2}},{breakpoint:601,settings:{slidesToShow:1}}]})}));
--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -4,6 +4,20 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     prevArrow: false,
-    nextArrow: false
+    nextArrow: false,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
   });
 });
